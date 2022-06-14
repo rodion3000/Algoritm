55a84edd56d1ea370034f459127914cff7ade5c7
rodion3000 <rodion.papizh@mail.ru>
2022-06-14 15:27:12 +0500

    Добавьте файлы проекта.

---
+++ Algoritm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Algoritm
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите ваш логин");
+            User.Login = Console.ReadLine();
+            Console.WriteLine("Введите ваше имя");
+            User.Name = Console.ReadLine();
+            Console.WriteLine($"Приветствую {User.Name}, ваш логин {User.Login} ");
+            Console.WriteLine("Имеете ли вы премиум подписку?");
+            User.Premium = Console.ReadLine();
+            if (User.Premium == "нет")
+            { User.ShowAds(); }
+
+
+        }
+
+    }
+
+
+
+}
